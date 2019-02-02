2a02b74342f2a1f03b5bd134f262404ad8873b9d
Ankit Singhal <ansingha@microsoft.com>
2019-02-02 12:51:07 +0000

    vars

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "loc" {
     description = "Default Azure region"
-    default     =   "West Europe"
+    default     =   "westeurope"
 }
 
 variable "tags" {
@@ -9,3 +9,8 @@ variable "tags" {
         env     = "training"
     }
 }
+variable "webapplocs" { 
+     description = "List of locations for web apps" 
+     type        = "list" 
+     default     = [] 
+} 
